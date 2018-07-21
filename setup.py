4ea89244f5b208bfd5f98b4b782e072a7729d57a
Alberto Sottile <alby128@gmail.com>
2018-07-22 01:07:31 +0200

    Encode path in setup.py

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ extra_link_args = []
 
 extra_compile_args.append("-IC:\\Program Files\\Microsoft SDKs\\Windows\\v7.0\\Include")
 extra_compile_args.append("-IC:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\include")
+extra_compile_args.append("-IC:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.10586.0\\ucrt")
 
 if 0:
     # enable this to debug a release build
